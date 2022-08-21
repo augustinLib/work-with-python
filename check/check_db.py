d9a06d057c14f6a4de23eca957ede49a3ddeb1e6
2bambitious <2bambitious@gmail.com>
2022-08-21 21:05:16 +0900

    Feat: Add DBcheck
    내부 DB누락인원 확인 기능 추가

---
+++ check/check_db.py
@@ -0,0 +1,53 @@
+import pandas as pd
+
+
+class CheckDB:
+    def __init__(self, inner_db, surveybox):
+        self.exception_list = []
+        self.inner_list = []
+        self.surveybox_list = []
+        self.exception_table = pd.read_excel("./data/exception_list.xlsx")
+
+        # exception_list 생성
+        for i in range(len(self.exception_table)):
+            inner_name_number = (self.exception_table.iloc[i,0], self.exception_table.iloc[i,1])
+            self.exception_list.append(inner_name_number)
+
+        # 각 DB별 list 생성
+        self.inner_db =  pd.read_excel(inner_db)
+        self.surveybox = pd.read_excel(surveybox)
+
+        self.inner_db = self.inner_db.iloc[:, [1,2]]
+        self.inner_db.columns = ["Q1", "Q2"]
+        self.inner_db.dropna(axis=0, inplace=True)
+        self.inner_db.reset_index(inplace=True, drop=True)
+
+        self.surveybox_person = self.surveybox[["Q1", "Q2"]]
+
+        for i in range(len(self.inner_db)):
+            inner_name_number = (self.inner_db.iloc[i,0], self.inner_db.iloc[i,1])
+            self.inner_list.append(inner_name_number)
+
+        for i in range(len(self.surveybox_person)):
+            survey_name_number = (self.surveybox_person.iloc[i,0], self.surveybox_person.iloc[i,1])
+            self.surveybox_list.append(survey_name_number)    
+
+    # 외부 DB에 있는데 내부 DB에 없는 사람 조회 후 결과값 반환
+    def return_result(self):
+        count = 0
+        missing_list = []
+        print("검사 시작 : 아래 목록의 사람들이 내부 DB에 없습니다")
+        for i, person in enumerate(self.surveybox_list):
+            if person not in self.inner_list:
+                if person not in self.exception_list:
+                    print(person)
+                    missing_list.append(person)
+
+                    count += 1
+
+            
+        print("총 %d명 누락됨" %count)
+        return missing_list
+
+
+
--- main.py
+++ main.py
@@ -1,33 +1,45 @@
 import argparse
-from distutils.command.config import config
-
 from send_notice.divive_num import DivideNum
+from check.check_db import CheckDB
 
-def define_parser():
-    p = argparse.ArgumentParser()
-
-    # --mode : 필요한 기능에 따라 모드 설정(필수 입력)
-    # --mode 1 : 단체문자 전송을 위한 전화번호 쪼개기
-    p.add_argument('--mode', required=True)
-
-    # --raw_data : 전화번호가 담겨있는 원본 파일 경로
-    # --output : 전화번호를 단체문자로 돌릴 수 있게끔 변환한 파일을 내보낼 경로
-    p.add_argument('--raw_data')
-    p.add_argument('--output')
-
-    config = p.parse_args()
-    return config
+# argparser보다 input으로 mode 설정하는것이 더 효율적이라고 판단하여 교체
+def define_mode():
+    print("사용할 기능을 입력하세요")
+    print("1 : 단체문자 전송")
+    print("2 : 내부 DB 검사")
+    
+    mode = input("")
+    return mode
 
 
 def main():
-    config = define_parser()
+    mode = define_mode()
     # mode 1 : 단체문자 전송을 위한 전화번호 쪼개기
-    if config.mode == "1":
-        divide = DivideNum(config.raw_data, config.output)
-        divide.convert_number()
-        divide.export()
+    if mode == "1":
+        print("------------------------------")
+        raw_data = input("원본 파일의 경로를 입력해주세요 : ")
+        output = input("내보낼 파일의 경로와 이름을 지정해주세요 : ")
+        divide_num = DivideNum(raw_data, output)
+        divide_num.convert_number()
+        divide_num.export()
+
+        print("------------------------------")
+        print("전화번호 추출 완료")
+        return
+
+    if mode == "2":
+        print("------------------------------")
+        inner_db = input("내부 DB 파일의 경로를 입력해주세요 : ")
+        out_db = input("외부 DB 파일의 경로를 입력해주세요 : ")
+        check_db = CheckDB(inner_db, out_db)
+        check_db.return_result()
+
+        print("------------------------------")
+        print("내부 DB 검사 완료")
+
         return
         
+
 
 
 
