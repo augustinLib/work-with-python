ccc0d1eb1aee04d22b06cfb1c59787e0cb7a1978
augustinLib <2bambitious@gmail.com>
2022-09-06 11:24:41 +0900

    Feat: Add actorCompare.js
    Google Apps Script를 이용하여 스프레드시트 상에서 주/야간 중복값 제외한 값을 반환해주는 함수 추가

--- actorCompare.js
+++ actorCompare.js
@@ -1,5 +1,35 @@
-function actorNum(start, finish) {
+/** @OnlyCurrentDoc */
+
+// 속성 입력값은 2씩 커짐
+// 따라서 30부터 시작한다고 하면, 30, 32 한 세트로 묶어서 비교 후 값 return
+
+
+function actorNum(range) {
+    var doc = SpreadsheetApp.getActive();
+    var sheet = doc.getSheetByName("데이터 수집 관리 파일(수정)");
+    var scope = sheet.getRange(range).activate();
+    var readData = scope.getValues()
+    var result = 0;
+    var tempList = [];
+  
+  
+    for (var i = 0; i < readData.length; i++) {
+      tempList.push(readData[i][0])
+    }
+    for (var index = 0; index <= tempList.length - 4; index += 4) {
+      if (tempList[index] == 1 && tempList[index+2] == 1) {
+        result += 1;
+      }
+  
+      else if (tempList[index] == 1 && tempList[index+2] == 0) {
+        result += 1;
+      }
+  
+      else if (tempList[index] == 0 && tempList[index+2] == 1) {
+        result += 1;
+      }
+      
+    }
     
-
-
-}
+    return result;
+  }
